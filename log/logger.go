7635aa87f68b6f4fcb2159fe590aefeb4768df4e
xinli1dragonplus <xin.li1@dragonplus.com>
2025-05-06 10:40:44 +0800

    logger

--- log/logger.go
+++ log/logger.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Logger interface {
 	Output(level string, s string)
+	CanLog(level string) bool
 }
 
 var logger Logger
@@ -13,28 +14,28 @@ func SetLogger(l Logger) {
 }
 
 func Debug(format string, a ...interface{}) {
-	if logger == nil {
+	if logger == nil || !logger.CanLog("debug") {
 		return
 	}
 	logger.Output("debug", fmt.Sprintf(format, a...))
 }
 
 func Info(format string, a ...interface{}) {
-	if logger == nil {
+	if logger == nil || !logger.CanLog("info") {
 		return
 	}
 	logger.Output("info", fmt.Sprintf(format, a...))
 }
 
 func Warn(format string, a ...interface{}) {
-	if logger == nil {
+	if logger == nil || !logger.CanLog("warn") {
 		return
 	}
 	logger.Output("warn", fmt.Sprintf(format, a...))
 }
 
 func Error(format string, a ...interface{}) {
-	if logger == nil {
+	if logger == nil || !logger.CanLog("error") {
 		return
 	}
 	logger.Output("error", fmt.Sprintf(format, a...))
